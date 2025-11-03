5332a3d17ece534bdabcf7b93fb46355fdf0b338
Narian <narian.angel@gmail.com>
2025-11-03 01:44:14 +0100

    Ajustes para poder sobreescribir estilos. Eliminados ficheros innecesarios.

---
+++ projects/nui/src/lib/nui.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nui-lib',
+  template: `
+    <p>
+      nui works!
+    </p>
+  `,
+  styles: []
+})
+export class NuiComponent {
+
+}
--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// Importar la librería NUI (componentes Angular si los hubiera)
+import { NuiService } from 'nui';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'playground';
+  title = 'NUI Playground - Usando librería instalada';
+
+  constructor(private nuiService: NuiService) {
+    // Usar el servicio de la librería
+    console.log('NUI Service disponible:', this.nuiService);
+  }
+  
+  toggleTheme() {
+    document.body.classList.toggle('theme-dark');
+  }
 }
